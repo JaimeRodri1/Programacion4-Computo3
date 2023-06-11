67ffc56ee4a115cf65db29894b5977059dfa3667
JaimeRodri1 <correostudent12@gmail.com>
2023-06-10 19:13:18 -0600

    agrego de practicas al repositorio

---
+++ Api_laravel/app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function getExcerptAttribute(){
+        return substr($this->content,o,150);
+    }
+
+    public function getPublishedAttribute(){
+        return $this->created_at->format/('d/m/Y');
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
